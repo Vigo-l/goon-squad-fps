4539db9708fceafa9de461dd03a39ec2d470fd6a
Vigo-l <v.linders@sintlucasedu.nl>
2023-11-28 14:26:01 +0100

    goon squad

---
+++ Assets/Scripts/SceneChanger.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonScript : MonoBehaviour
+{
+    public void home()
+    {
+        SceneManager.LoadScene("Home");
+    }
+}
